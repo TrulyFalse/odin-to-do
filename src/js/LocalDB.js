81bc32d41d8098e365d5dd2f576db3978c0be121
Abrar Ur Alam <safee20142@gmail.com>
2026-06-18 00:18:35 +0600

    Minor changes

--- src/js/LocalDB.js
+++ src/js/LocalDB.js
@@ -14,13 +14,13 @@ function getProjects(){return projects.slice();}
 function addProject(project){projects.push(project);}
 function removeProject(index) {
     if(projects[index].name === "General")
-        throw new Error("Not allowed to delete 'General' project.");
+        throw new Error("'General' project is not deletable as it constitutes the primary folder for storing To-Dos.");
     projects.splice(index, 1);
 }
 
 function serialize(){localStorage[LOCAL_STORAGE_KEY] = JSON.stringify(projects);}
 function deserialize(){
-    let rawProjects = JSON.parse(localStorage[LOCAL_STORAGE_KEY]); // "raw" because they are just objects storing project attributes but lacking the methods an actual Project instance would have
+    let rawProjects = JSON.parse(localStorage[LOCAL_STORAGE_KEY]); // "raw" because these objects only store project's attributes but lack methods an actual Project instance would have (since JSON can't store functions)
     for(let rawProject of rawProjects)
         projects.push(new Project(rawProject));
 }
