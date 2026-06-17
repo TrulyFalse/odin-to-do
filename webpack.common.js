d1ecad93b335b68ebeee9018df0d0ccd61694054
Abrar Ur Alam <safee20142@gmail.com>
2026-06-17 12:10:51 +0600

    Setup config files and directories

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: {
+        app: "./src/js/app.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|webp|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            }
+        ]
+    }
+};
